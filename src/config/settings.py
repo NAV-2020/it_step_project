cdf4a216a84c4d1db43f86166f85310ad19a5773
Nichipurenko <na202867@gmail.com>
2020-11-08 23:34:44 +0200

    Worked with django template and models

--- src/config/settings.py
+++ src/config/settings.py
@@ -115,8 +115,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+#Collect static
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_URL = '/static/'
--- src/core/views.py
+++ src/core/views.py
@@ -1,16 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
+
+
 def home(request, *args, **kwargs) -> HttpResponse:
     """Home view"""
-
-    ctx = {
-        'messages': ["This is message 1",
-                    "This is message 2",
-                    "This is message 3",
-                    "This is message 4"
-        ] 
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
     }
-
-    return render(request, 'core/index.html', context=ctx)
+    return render(request, 'core/index.html', context=context)
 
