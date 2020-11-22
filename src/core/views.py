720c11717bfd76ead529d022f8a8fda9eaeefa9d
Nichipurenko <na202867@gmail.com>
2020-11-22 12:18:38 +0200

    Added registration part1
    Added authorization

--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 from .models import Post
 
@@ -9,6 +9,11 @@ class HomeVievs(ListView):
     """Home view"""
     template_name = 'core/index.html'
     queryset = Post.objects.filter(is_active=True)
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        return super().get(request, *args, **kwargs)
 
 class PostsVievs(ListView):
     """Posts view"""
